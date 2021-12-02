01564dca96b0016a05519712a97d65677fe4355e
Tunahan Tezcan <ttezcan.1999@gmail.com>
2021-12-02 20:20:39 +0300

    post model added

---
+++ server/models/Post.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const PostSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+
+export default Post;
