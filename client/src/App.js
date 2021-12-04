1afa508db0b045375f93056445cc1a55a58b8b47
Tunahan Tezcan <ttezcan.1999@gmail.com>
2021-12-04 11:30:25 +0300

    some frontend changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
+import Home from "./pages/home/Homegit";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
