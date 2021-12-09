6cc3520f4cba722c697bd2c3b6c7cd6c225a8b0f
Tunahan Tezcan <ttezcan.1999@gmail.com>
2021-12-09 18:59:38 +0300

    m

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,19 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <Register />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
