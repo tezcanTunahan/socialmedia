a040d25d6a2d5e9cd571115d7b5a02f86767d152
Tunahan Tezcan <ttezcan.1999@gmail.com>
2021-12-28 18:53:09 +0300

    sidebar

--- client/src/components/closeFriend/CloseFriend.jsx
+++ client/src/components/closeFriend/CloseFriend.jsx
@@ -1,14 +1,9 @@
 import "./closeFriend.css";
 
-export default function CloseFriend() {
+export default function CloseFriend({ friend }) {
   return (
     <li className="sidebarfriend">
-      <img
-        className="sidebarFriendImg"
-        src="https://tr.web.img4.acsta.net/c_310_420/pictures/19/10/02/21/34/5281471.jpg"
-        alt=""
-      />
-      <span className="sidebarFriendName">spiderman</span>
+      <span className="sidebarFriendName">{friend.userName}</span>
     </li>
   );
 }
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,47 +1,33 @@
 import "./sidebar.css";
 import CloseFriend from "../closeFriend/CloseFriend";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const user = useContext(AuthContext);
+  const [userFriends, setUserFriends] = useState([]);
+
+  useEffect(() => {
+    const getFriends = async () => {
+      const res = await axios.get(
+        `http://localhost:5000/api/users/friends/${user.user._id}`
+      );
+      setUserFriends(res.data);
+    };
+    getFriends();
+  });
+
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
-        <ul className="sidebarList">
-          <li className="sidebarListItem">
-            <i className="fas fa-rss"></i>
-          </li>
-          <li className="sidebarListItem">
-            <i className="far fa-comment-dots"></i>
-          </li>
-          <li className="sidebarListItem">
-            <i className="far fa-play-circle"></i>
-          </li>
-          <li className="sidebarListItem">
-            <i className="fas fa-user-friends"></i>
-          </li>
-          <li className="sidebarListItem">
-            <i className="fas fa-bookmark"></i>
-          </li>
-          <li className="sidebarListItem">
-            <i className="far fa-question-circle"></i>
-          </li>
-          <li className="sidebarListItem">
-            <i className="fas fa-briefcase"></i>
-          </li>
-          <li className="sidebarListItem">
-            <i className="fas fa-calendar-alt"></i>
-          </li>
-          <li className="sidebarListItem">
-            <i className="fab fa-redhat"></i>
-          </li>
-        </ul>
-        <button className="sidebarButton">show more</button>
+        <span>Friends</span>
         <hr className="sidebarHr" />
         <ul className="sidebarFriendList">
-          <CloseFriend />
-          <CloseFriend />
-          <CloseFriend />
-          <CloseFriend />
-          <CloseFriend />
+          {userFriends.map((friend) => {
+            return <CloseFriend friend={friend} />;
+          })}
         </ul>
       </div>
     </div>
